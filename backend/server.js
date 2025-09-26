5b8850cb9f789892ebfd731dd2bcf32d6d33cc05
Nitin Bhandari <nitin211206@gmail.com>
2025-09-26 13:50:05 +0530

    feat: Backend2

--- backend/server.js
+++ backend/server.js
@@ -11,14 +11,10 @@ app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGODB_URL)
   .then(() => console.log("✅ MongoDB Connected"))
   .catch((err) => console.error("❌ MongoDB Error:", err));
 
 app.use("/api", userRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+export default app;
